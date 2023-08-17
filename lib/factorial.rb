460dd55bb5967e810037ef6319a72105275492ba
Philipe <dev.philipe@outlook.com>
2023-08-17 08:39:31 -0300

    refactor(factorial): method refactored using reduce

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,14 +1,5 @@
 def factorial(n)
-  if n < 0
-    nil
-  elsif n == 0
-    1
-  else
-    i = n - 1
-    while i >= 1
-      n = n * i
-      i -= 1
-    end
-    n
-  end
+  return nil if n < 0
+  return 1 if n == 0
+  (1..n).reduce(1, :*)
 end
